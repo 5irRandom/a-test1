4933b433dc26ce41e25273fafb18fb43a4ede832
Jaxon D <duncan5irjax@gmail.com>
2021-03-10 21:04:52 -0600

    fixed everything

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('cum');
+    res.json(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {console.log(`App listening on port ${PORT}`)});
