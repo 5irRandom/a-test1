863765c802ca83c015137e6a5168548ecbe464e0
Jaxon D <duncan5irjax@gmail.com>
2021-03-10 21:07:00 -0600

    Actually fixed

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.json(path.join(__dirname, "index.html"));
+    return fs.readFile(path.join(__dirname, "index.html"), (e, data) => {
+        if (e) console.error(e);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      })
 });
 
 app.listen(PORT, () => {console.log(`App listening on port ${PORT}`)});
